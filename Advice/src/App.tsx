2928742cc403bbb716bc7c98aad81e8e32acd944
unknown <kidus.muluwork@gmail.com>
2023-08-22 04:21:27 +0300

    Created Pizza-Menu react app

--- Advice/src/App.tsx
+++ Advice/src/App.tsx
@@ -1,23 +1,20 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-
-
 function App() {
   const [advice, setadvice] = useState("");
-  const [count, setcount]= useState(0);
+  const [count, setcount] = useState(0);
 
   async function giveAdvice() {
     const res = await fetch(`http://api.adviceslip.com/advice`);
     const data = await res.json();
     setadvice(data.slip.advice);
-    setcount((c) => c = c+1);
+    setcount((c) => (c = c + 1));
   }
 
-  
-useEffect(()=>{
-  giveAdvice();
-}, []);
+  useEffect(() => {
+    giveAdvice();
+  }, []);
 
   return (
     <>
@@ -25,17 +22,19 @@ useEffect(()=>{
         <h1>"{advice}"</h1>
         <button onClick={giveAdvice}>&gt;</button>
       </div>
-      <Message count={count}/>
+      <Message count={count} />
     </>
   );
 }
 
 export default App;
 
-function Message({count}:any){
-  return(
+function Message({ count }: any) {
+  return (
     <div className="Message">
-      <p>You have viewd <span>{count}</span> Advice</p>
+      <p>
+        You have viewd <span>{count}</span> Advice
+      </p>
     </div>
   );
 }
