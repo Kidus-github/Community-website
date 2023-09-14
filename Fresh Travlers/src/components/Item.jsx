50a610812c9a0c0e458aad48c9e7dcc27073218d
unknown <kidus.muluwork@gmail.com>
2023-09-14 20:57:59 +0300

    added sorting features on the list

--- Fresh Travlers/src/components/Item.jsx
+++ Fresh Travlers/src/components/Item.jsx
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
 
 function Item({ item, onToggleItem, onDeleteItems }) {
-  const [packed, setPacked] = useState(false);
   return (
-    <div className="item">
-      <input
-        type="checkbox"
-        value={packed}
-        onChange={() => onToggleItem(item.id)}
-      />
-      <span>
-        {"  "}
-        {item.qty} {item.discription}
-      </span>
+    <div className="itemlist">
+      <div>
+        <input
+          type="checkbox"
+          value={item.packed}
+          onChange={() => onToggleItem(item.id)}
+        />
+        <span>
+          {"  "}
+          {item.qty} {item.discription}
+        </span>
+      </div>
       <button onClick={() => onDeleteItems(item.id)}>Del</button>
     </div>
   );
--- Fresh Travlers/src/components/PackingList.jsx
+++ Fresh Travlers/src/components/PackingList.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import CategoryCard from "../components/CategoryCard";
 
 function PackingList({
@@ -7,21 +8,51 @@ function PackingList({
   onClearItems,
   onToggleItem,
 }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItem;
+
+  switch (sortBy) {
+    case "packed": {
+      sortedItem = packedItem
+        .slice()
+        .sort((a, b) => Number(a.packed) - Number(b.packed));
+      console.log(sortedItem);
+      break;
+    }
+    case "description": {
+      sortedItem = packedItem
+        .slice()
+        .sort((a, b) => a.discription.localeCompare(b.discription));
+      console.log(sortedItem);
+      break;
+    }
+    case "category": {
+      sortedItem = packedItem
+        .slice()
+        .sort((a, b) => a.category.localeCompare(b.category));
+      console.log(sortedItem);
+      break;
+    }
+    default: {
+      sortedItem = packedItem;
+    }
+  }
   return (
     <div className="packingList">
       <div className="packlist_header">
         <button onClick={onClearItems}>Clear All</button>
-        <select>
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
           <option disabled>Sort By</option>
           <option value="ByInput">Sort By Input</option>
-          <option value="ByDiscription">Sort By Discription</option>
+          <option value="description">Sort By Description</option>
           <option value="ByCategory">Sort By Category</option>
         </select>
       </div>
 
-      {packedItem.length !== 0 ? (
+      {sortedItem.length !== 0 ? (
         <div className="packlist_body">
-          {[...new Set(packedItem.map((item) => item.category))].map(
+          {[...new Set(sortedItem.map((item) => item.category))].map(
             (item, i) => (
               <CategoryCard
                 key={i}
